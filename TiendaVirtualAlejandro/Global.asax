07a5cb9a02e271f19cffa29177d0fb3255f76e65
Alejandro <alem2518@hotmail.com>
2020-02-22 09:30:19 +0100

    Initial commit

---
+++ TiendaVirtualAlejandro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TiendaVirtualAlejandro.MvcApplication" Language="C#" %>
---
+++ TiendaVirtualAlejandro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TiendaVirtualAlejandro.Startup))]
+namespace TiendaVirtualAlejandro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
